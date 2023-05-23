c2f75f980d7777b42883b59292fe29e046fe26b5
aya-matroud <ayamatroud8@gmail.com>
2023-05-23 03:56:22 -0700

    addLogin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,30 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+            $user = new User();
+            $user->first_name = 'aya';
+            $user->last_name = 'matroud';
+            $user->phone = '+963937158233';
+            $user->email = 'aya@gmail.com';
+            $user->password = Hash::make(123456789);
+            $user->save();
+
+
     }
+
 }
