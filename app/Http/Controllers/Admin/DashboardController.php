8771a17eea439200c96c37ed2ebb65262e17f603
mina <mina899432@gmail.com>
2023-06-07 23:05:07 +0300

    admin v1

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -18,7 +18,7 @@ class DashboardController extends Controller
         $data['user_count'] = User::count();
         $data['offer_count'] = Offer::whereStatus(1)->count();
         $data['voucher_count'] = Voucher::count();
-        $data['subs'] = Category::sub->withCount('services')->get();
+        $data['subs'] = Category::sub()->withCount('services')->get();
         $data['categories'] = Category::with(['subcategories'=>function($q)use($data){
             $q->whereIn('parent_id',$data['subs']->pluck('id')->toArray());
         }])->parent()->get();
