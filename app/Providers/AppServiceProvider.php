2cafab310739aa668a92811169357f067bcf94ec
mina <mina899432@gmail.com>
2023-06-07 12:13:57 +0300

    admin v1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        dd(Request::getHost());
+        if(Request::getHost()=="goldencard.com.jo")
+            config('app.asset_alt','main/public');
     }
 }
