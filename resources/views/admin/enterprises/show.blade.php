bf1da032995933873a159346c9d1ad0bf845feac
mina <mina899432@gmail.com>
2023-06-08 13:46:00 +0300

    admin v1

--- resources/views/admin/enterprises/show.blade.php
+++ resources/views/admin/enterprises/show.blade.php
@@ -6,7 +6,7 @@
         <!-- row -->
         <div class="page-titles">
             <ol class="breadcrumb">
-                <li><h5 class="bc-title">{{ $subscription->entreprise_name }}</h5></li>
+                <li><h5 class="bc-title">{{ $subscription->enterprise_name }}</h5></li>
                 <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">
                     <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M2.125 6.375L8.5 1.41667L14.875 6.375V14.1667C14.875 14.5424 14.7257 14.9027 14.4601 15.1684C14.1944 15.4341 13.8341 15.5833 13.4583 15.5833H3.54167C3.16594 15.5833 2.80561 15.4341 2.53993 15.1684C2.27426 14.9027 2.125 14.5424 2.125 14.1667V6.375Z" stroke="#2C2C2C" stroke-linecap="round" stroke-linejoin="round"/>
@@ -14,7 +14,7 @@
                     </svg>
                     Home </a>
                 </li>
-                <li class="breadcrumb-item active"><a href="javascript:void(0)">{{ $subscription->entreprise_name }} </a></li>
+                <li class="breadcrumb-item active"><a href="javascript:void(0)">{{ $subscription->enterprise_name }} </a></li>
             </ol>
             <a class="text-primary fs-13" href="{{ route('admin.enterprises.index') }}" >{{  __('Enterprise Subscriptions') }}</a>
         </div>
@@ -25,7 +25,7 @@
                         <div class="card-body p-0">
                             <div class="offcanvas-body">
                                 <div class="container-fluid">
-                                <h4 class="heading mb-5"> {{ $subscription->entreprise_name }}</h4>
+                                <h4 class="heading mb-5"> {{ $subscription->enterprise_name }}</h4>
 
                                     <p class="mb-3"><strong>Start Date : </strong> {{ $subscription->start_date }}</p>
                                     <p class="mb-3"><strong>End Date : </strong> {{ $subscription->end_date }}</p>
