3c2b42aa10e63603b2244920bd745473eb6f9935
mina <mina899432@gmail.com>
2023-06-08 13:31:22 +0300

    admin v1

--- app/Http/Controllers/Admin/EnterpriseCoponeController.php
+++ app/Http/Controllers/Admin/EnterpriseCoponeController.php
@@ -12,7 +12,7 @@ class EnterpriseCoponeController extends Controller
 {
     public function index()
     {
-        $data = EnterpriseSubscription::whereDate('end_date','<=',Carbon::now()->today())->with('copones')->latest()->paginate(10);
+        $data = EnterpriseSubscription::whereDate('end_date','<=',Carbon::today())->with('copones')->latest()->paginate(10);
         dd($data);
         return view('admin.enterprise-copones.index',compact('data'));
     }
