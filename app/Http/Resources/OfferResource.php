ed13654b7853b68e352b5327ad36a0731bc2eb0c
aya-matroud <ayamatroud8@gmail.com>
2023-06-19 01:30:33 -0700

    editFav

--- app/Http/Resources/OfferResource.php
+++ app/Http/Resources/OfferResource.php
@@ -3,6 +3,9 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use App\Models\Voucher;
+use Auth;
+use App\Models\Favorite;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OfferResource extends JsonResource
@@ -14,9 +17,19 @@ class OfferResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $fav = false;
+        if(Auth::user()){
+        $favorite = Favorite::where('user_id',Auth::user()->id)->where('offer_id',$this->id)->first();
+            if($favorite){
+                $fav = true ;
+            }
+        }
+
+
         return [
 
             'id' => $this->id,
+            'is_favorite'=>$fav,
             'name' => $this->name,
             'description' => $this->description,
             'discount_value'=> $this->discount_value,
@@ -25,6 +38,7 @@ class OfferResource extends JsonResource
             'status' => $this->status,
             'end_date' => $this->end_date,
             'use_times' => $this->use_times,
+            'sum_uses'=> Voucher::where('offer_id', $this->id)->count(),
             'service_id' => $this->service?->id,
             'service_name' => $this->service?->name,
             'branches' => BranchResource::collection($this?->branches),
