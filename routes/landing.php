aab3dca931d5cd160d7e37d9c0bedf86930b73a0
mina <mina899432@gmail.com>
2023-07-01 14:22:35 +0300

    landing

--- routes/landing.php
+++ routes/landing.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Landing\LandingController;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::group([/*'prefix' => LaravelLocalization::setLocale(),*/
+Route::group(['prefix' => LaravelLocalization::setLocale(),
     'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {
         Route::get('/',[LandingController::class, 'getLanding'])->name('landing');
         Route::get('/about-us',[LandingController::class, 'aboutUs'])->name('about');
