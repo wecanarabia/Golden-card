7026ffaa9b2f507893a27f7e72c50f02b7527138
mina <mina899432@gmail.com>
2023-07-02 17:44:42 +0300

    landing

--- app/Traits/NotificationTrait.php
+++ app/Traits/NotificationTrait.php
@@ -24,7 +24,7 @@ trait NotificationTrait
                 // 'registration_ids' => $token,
                 'to'=>'/topics/all',
                 'notification' => $msg,
-                // 'time'=> Carbon::parse($datetime),
+                'time'=> Carbon::parse($datetime),
 
             );
         } else {
