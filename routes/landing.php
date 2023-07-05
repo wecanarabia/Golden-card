fe1a6d0f78fbab2e90aadded5584cf991a126434
mina <mina899432@gmail.com>
2023-07-05 12:30:14 +0300

    changes1

--- routes/landing.php
+++ routes/landing.php
@@ -4,14 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Landing\LandingController;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-// Route::group(['prefix' => LaravelLocalization::setLocale(),
-//     'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {
-//         Route::get('/',[LandingController::class, 'getLanding'])->name('landing');
-//         Route::get('/about-us',[LandingController::class, 'aboutUs'])->name('about');
-//         Route::get('/privacy-policy',[LandingController::class, 'privacyPolicy'])->name('privacy');
-//         Route::get('/terms-conditions',[LandingController::class, 'termsConditions'])->name('conditions');
+Route::group(['prefix' => LaravelLocalization::setLocale(),
+    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']], function () {
+        Route::get('/',[LandingController::class, 'getLanding'])->name('landing');
+        Route::get('/about-us',[LandingController::class, 'aboutUs'])->name('about');
+        Route::get('/privacy-policy',[LandingController::class, 'privacyPolicy'])->name('privacy');
+        Route::get('/terms-conditions',[LandingController::class, 'termsConditions'])->name('conditions');
 
-//         Route::get('/{any}', function($any){
-//             return abort('406');
-//         })->where('any', '.*');
-// });
+        Route::get('/{any}', function($any){
+            return abort('406');
+        })->where('any', '.*');
+});
