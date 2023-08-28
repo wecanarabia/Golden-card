06a4379ef34cadb3375bae96607ab1fcd763f44c
mina <mina899432@gmail.com>
2023-08-29 00:58:20 +0300

    fix

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -43,7 +43,7 @@ class ServiceController extends Controller
     public function create()
     {
         $request = app(Request::class);
-        $ip = $request->ip();
+        $ip = $request()->ip();
         dd($ip);
         $currentUserInfo = Location::get($ip);
 
