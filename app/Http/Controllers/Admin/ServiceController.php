f3fe0e930b7a6b5f4cdf11c7c50d112227e9ecd5
mina <mina899432@gmail.com>
2023-08-29 14:12:58 +0300

    fix

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -44,6 +44,7 @@ class ServiceController extends Controller
      */
     public function create()
     {
+        dd($_SERVER);
         $subcategories = Subcategory::get();
         return view('admin.services.create',compact('subcategories'));
     }
