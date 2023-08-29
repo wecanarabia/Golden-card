cfcd5c4d6c9b788e777c58d651981d4ff3c76171
mina <mina899432@gmail.com>
2023-08-29 06:33:56 +0300

    fix

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -44,7 +44,7 @@ class ServiceController extends Controller
      */
     public function create()
     {
-        $ipAddress = file_get_contents('https://api.ipify.org/v1/ip');
+        $ipAddress = file_get_contents('https://api.ipify.org?format=json');
         dd($ipAddress);
         $currentUserInfo = Location::get($ipAddress);
         dd($currentUserInfo);
