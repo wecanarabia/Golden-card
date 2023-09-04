97216a1d5a2117a445a4c631a15872332293b2a7
aya-matroud <ayamatroud8@gmail.com>
2023-09-04 00:55:54 -0700

    fixed

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -11,9 +11,9 @@ class Notification extends Model
 
     protected $guarded = [];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+    // public function user()
+    // {
+    //     return $this->belongsTo(User::class);
+    // }
 
 }
