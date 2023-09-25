7918b9dd22da3f9a4eedc3b250c1f0db1e58cecb
mina <mina899432@gmail.com>
2023-09-25 22:04:10 +0300

    new

--- app/Http/Controllers/Admin/OfferController.php
+++ app/Http/Controllers/Admin/OfferController.php
@@ -25,8 +25,9 @@ class OfferController extends Controller
             $services = Service::where('admin_id',Auth::user()->id)->latest()->get();
             if (is_null($services)) {
                 $data=collect([]);
+            }else{
+                $data = Offer::latest()->with('service')->orderBy('service_id')->whereBelongsTo($services)->get();
             }
-            $data = Offer::latest()->with('service')->orderBy('service_id')->whereBelongsTo($services)->get();
         }
         return view('admin.offers.index',compact('data'));
     }
